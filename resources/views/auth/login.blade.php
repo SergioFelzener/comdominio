533ba8ec4572957d017cc4a8a28b91bd8db81b33
Sergio Felzener <felzenerdj@gmail.com>
2020-10-28 18:13:22 -0300

    finish login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,38 @@
 <x-guest-layout>
     <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+        <x-slot name="logo"></x-slot>
 
         <x-jet-validation-errors class="mb-4" />
 
         @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Login') }}
-                </x-jet-button>
-            </div>
-        </form>
-        <div>
-            <a href="{{route('social.login', ['provider' => 'github'])}}">Login Github</a>
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
         </div>
+        @endif
+        <div class="flex">
+            <form method="POST" action="{{ route('login') }}" class="max-w-sm mx-auto shadow-lg bg-white rounded-lg pt-6 pb-8 mb-4 px-8">
+                @csrf
+                <img class="" src="{{asset('assets/img/logo.png')}}">
+                <div class="">
+                    <div class="my-4 max-w-sm text-center">
+                        <h2 class="my-2 font-bold">Login com Social Media</h2>
+                        <a href="{{route('social.login', ['provider' => 'github'])}}" class="p-2 flex justify-center font-bold border-solid border-2 border-gray-600 mr-auto ml-auto bg-gray-300 rounded-2xl shadow-2xl text-black hover:bg-gray-100">
+                        <img class="w-12 h-12 mr-4" src="{{asset('assets/img/gitlogo.png')}}">
+                            <label class="text-bold mt-3"for="">GitHub Login</label>
+                        </a>
+                    </div>
+                    <div class="my-4 max-w-sm text-center">
+                        <a href="{{route('social.login', ['provider' => 'facebook'])}}" class="p-2 flex justify-center font-bold border-solid border-2 border-gray-600 mr-auto ml-auto bg-blue-500 rounded-2xl shadow-2xl text-black hover:bg-blue-200">
+                        <img class="w-12 h-12 mr-4" src="{{asset('assets/img/face.png')}}">
+                            <label class="text-bold mt-3"for="">Facebook Login</label>
+                        </a>
+                    </div>
+                    <p class="flex justify-center  text-red-500 text-xs">
+                        Não tem uma conta,&nbsp; <a href="#" class="justify-center text-black-700 font-bold">Crie sua conta </a> &nbsp; agora.
+                    </p>
+                    
+            </form>
+        </div>
+
     </x-jet-authentication-card>
 </x-guest-layout>
